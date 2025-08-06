f2cb6e2a7bd872aa7805bcefae651e669927d446
chaudhary64 <chiragchoudhary64@gmail.com>
2025-08-07 03:28:28 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,43 @@ const Home = () => {
   return (
     <>
       <main className="w-[90%] max-w-[1440px] mx-auto">
-        <div className="mx-auto mt-20 text-center">
-          <p className="text-sm">ACHIEVE YOUR FITNESS GOALS</p>
-          <h1
-            style={{
-              lineHeight: 1,
-            }}
-            className="text-[7rem] font-bebas-neue"
-          >
-            FIND YOUR STRENGTH
-          </h1>
-        </div>
-        <div className="mx-auto mt-10 max-w-[1024px] h-[512px] rounded-xl overflow-hidden">
-          <img
-            src="https://images.unsplash.com/photo-1584863231364-2edc166de576?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Hero Image"
-            className="object-cover object-center h-full w-full"
-          />
-        </div>
+        <section className="mt-20">
+          <div className="mx-auto text-center">
+            <p className="text-sm">ACHIEVE YOUR FITNESS GOALS</p>
+            <h1
+              style={{
+                lineHeight: 1,
+              }}
+              className="text-[7rem] font-bebas-neue"
+            >
+              FIND YOUR STRENGTH
+            </h1>
+          </div>
+          <div className="mx-auto mt-10 max-w-[1024px] h-[512px] rounded-xl overflow-hidden">
+            <img
+              src="https://images.unsplash.com/photo-1584863231364-2edc166de576?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Hero Image"
+              className="object-cover object-center h-full w-full"
+            />
+          </div>
+        </section>
+
+        <section className="mt-20 lg:w-1/2 mx-auto flex flex-col items-center text-center gap-6">
+          <img src="/logo/Star Logo.svg" className="h-20" />
+          <div>
+            <h3 className="text-6xl font-bebas-neue">Fitness should be </h3>
+            <h3 className="text-6xl font-bebas-neue">Accessible to Everyone</h3>
+          </div>
+          <p>
+            CoreX is a modern fitness platform that connects you with top
+            trainers, personalized programs, and a community of fitness
+            enthusiasts. Whether you're a beginner or an experienced athlete,
+            CoreX has something for everyone.
+          </p>
+          <button className="py-2 px-10 rounded-full cursor-pointer border-2 border-black hover:bg-black hover:text-white transition-colors duration-700">
+            Join Now
+          </button>
+        </section>
       </main>
     </>
   );
