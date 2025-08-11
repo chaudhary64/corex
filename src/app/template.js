ed6764ee116c594d1afe88d47f605068b6f1cc05
chaudhary64 <chiragchoudhary64@gmail.com>
2025-08-11 18:52:11 +0530

    Add GSAP and Lenis for improved animations and scrolling effects in template

--- src/app/template.js
+++ src/app/template.js
@@ -1,13 +1,29 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import Nav from "./components/ui/Nav";
 import Footer from "./components/ui/Footer";
+import { ReactLenis } from "lenis/react";
+import gsap from "gsap";
 
 const template = ({ children }) => {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
   return (
     <>
-      <Nav />
-      {children}
-      <Footer />
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
+        <Nav />
+        {children}
+        <Footer />
+      </ReactLenis>
     </>
   );
 };
