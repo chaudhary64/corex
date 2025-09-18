0f3d82617317f782d5c1fe07149e8be5c0c2054b
chaudhary64 <chiragchoudhary64@gmail.com>
2025-09-18 21:40:54 +0530

    feat: enhance mobile navigation with GSAP animations and refactor state management

--- src/app/components/ui/Nav.js
+++ src/app/components/ui/Nav.js
@@ -1,20 +1,38 @@
 "use client";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
 import React, { useRef, useState } from "react";
 
 const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const logoRef = useRef(null);
   const btnDesktopRef = useRef(null);
+  const btnMobileRef = useRef(null);
   const desktopLinkRefs = useRef();
   const hamburgerRef = useRef(null);
+  const crossRef = useRef(null);
+  const mobileNavRef = useRef(null);
+  const mobileAsideRef = useRef(null);
+  const mobileDescRef = useRef(null);
 
   useGSAP(() => {
     let mm = gsap.matchMedia();
 
-    mm.add("(width < 64rem)", () => {
+    mm.add("(width < 64rem)", (context) => {
       // Mobile-specific animations
+
+      const mobileSpans = [...mobileNavRef.current.children].slice(1, 6);
+
+      let splitedMobileSpans = SplitText.create(mobileSpans, {
+        type: "lines, chars",
+        mask: "lines",
+      });
+      let splitedMobileDesc = SplitText.create(mobileDescRef.current, {
+        type: "lines, chars",
+        linesClass: "overflow-hidden block",
+        mask: "lines",
+      });
+
       let tl = gsap.timeline();
       tl.from(logoRef.current, {
         opacity: 0,
@@ -31,6 +49,54 @@ const Nav = () => {
         },
         0
       );
+
+      let showMobileNav = gsap.timeline({ paused: true });
+
+      showMobileNav
+        .to(mobileNavRef.current, {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "power2.InOut",
+          transformOrigin: "center center",
+        })
+        .from(crossRef.current, {
+          opacity: 0,
+        })
+        .from(splitedMobileSpans.chars, {
+          opacity: 0,
+          y: 20,
+          stagger: 0.02,
+        })
+        .from(btnMobileRef.current, {
+          opacity: 0,
+          duration: 0.75,
+        })
+        .from(
+          mobileAsideRef.current,
+          {
+            scaleX: 0,
+            transformOrigin: "left center",
+          },
+          "<"
+        )
+        .from(
+          splitedMobileDesc.chars,
+          {
+            opacity: 0,
+            stagger: 0.01,
+          },
+          "<"
+        );
+
+      context.add("open", () => {
+        showMobileNav.timeScale(1).play();
+      });
+
+      context.add("close", () => {
+        showMobileNav.timeScale(1.75).reverse();
+      });
+
+      hamburgerRef.current?.addEventListener("click", context.open);
+      crossRef.current?.addEventListener("click", context.close);
     });
 
     mm.add("(width >= 64rem)", () => {
@@ -78,6 +144,8 @@ const Nav = () => {
 
     return () => {
       mm.revert();
+      hamburgerRef.current?.removeEventListener("click", context.open);
+      crossRef.current?.removeEventListener("click", context.close);
     };
   }, []);
 
@@ -105,11 +173,7 @@ const Nav = () => {
         </button>
 
         {/* Toggle Mobile Navigation */}
-        <button
-          ref={hamburgerRef}
-          className="cursor-pointer lg:hidden"
-          onClick={() => setIsOpen(!isOpen)}
-        >
+        <button ref={hamburgerRef} className="cursor-pointer lg:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
@@ -127,50 +191,60 @@ const Nav = () => {
         </button>
 
         {/* Mobile Navigation */}
-        {isOpen && (
-          <div className="px-[5%] fixed z-10 inset-0 w-full bg-black flex flex-col justify-around items-center">
-            <button
-              className="self-end cursor-pointer lg:hidden flex items-center gap-2"
-              onClick={() => setIsOpen(!isOpen)}
+        <div
+          ref={mobileNavRef}
+          style={{
+            clipPath: "polygon(100% 0%, 100% 0%, 100% 0%, 100% 0%)",
+          }}
+          className="px-[5%] fixed z-10 inset-0 w-full bg-black flex flex-col justify-around items-center"
+        >
+          <button
+            ref={crossRef}
+            className="self-end cursor-pointer lg:hidden flex items-center gap-2"
+          >
+            <span className="font-semibold text-slate-200">close</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              className="h-10 w-10"
             >
-              <span className="font-semibold text-slate-200">close</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                className="h-10 w-10"
-              >
-                <path
-                  d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z"
-                  fill="#E2E8F0"
-                />
-              </svg>
-            </button>
-            <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
-              Home
-            </span>
-            <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
-              Trainers
-            </span>
-            <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
-              Programs
-            </span>
-            <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
-              Experiences
-            </span>
-            <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
-              Pricing
-            </span>
-            <button className="w-full bg-white py-2 rounded-full uppercase cursor-not-allowed">
-              Begin Your Journey
-            </button>
-            <aside className="w-full border-b border-slate-200"></aside>
-            <p className="text-slate-200 text-xs text-center">
-              We create transformative fitness journeys that celebrate the human
-              spirit, making every workout a step toward your true self.
-            </p>
-          </div>
-        )}
+              <path
+                d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z"
+                fill="#E2E8F0"
+              />
+            </svg>
+          </button>
+          <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
+            Home
+          </span>
+          <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
+            Trainers
+          </span>
+          <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
+            Programs
+          </span>
+          <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
+            Experiences
+          </span>
+          <span className="text-xl uppercase font-semibold text-slate-200 tracking-wider cursor-not-allowed">
+            Pricing
+          </span>
+          <button
+            ref={btnMobileRef}
+            className="w-full bg-white py-2 rounded-full uppercase cursor-not-allowed"
+          >
+            Begin Your Journey
+          </button>
+          <aside
+            ref={mobileAsideRef}
+            className="w-full border-b border-slate-200"
+          ></aside>
+          <p ref={mobileDescRef} className="text-slate-200 text-xs text-center">
+            We create transformative fitness journeys that celebrate the human
+            spirit, making every workout a step toward your true self.
+          </p>
+        </div>
       </nav>
     </header>
   );
