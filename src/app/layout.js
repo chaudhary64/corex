e53a1e10677bed386758eab835ef67bc2b191273
chaudhary64 <chiragchoudhary64@gmail.com>
2025-09-26 23:51:27 +0530

    fix: add background color to body in RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Mozilla+Headline:wdth,wght@75..125,200..700&display=swap" rel="stylesheet" />
   </head>
-  <body className={`${bebasNeue.variable}`}>
+  <body className={`${bebasNeue.variable} bg-[#EEEEEE]`}>
     {children}
   </body>
 </html>
