71434c500e7acf6cea2e431d62b0740f2c0dfc18
chaudhary64 <chiragchoudhary64@gmail.com>
2025-10-23 16:19:19 +0530

    refactor: update JSX structure in WhyUs component for improved readability

--- src/app/components/WhyUs.js
+++ src/app/components/WhyUs.js
@@ -16,9 +16,9 @@ const WhyUs = ({ src, heading, description, highlights, btnTxt, layout }) => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
   return (
-    <div className="flex max-lg:flex-wrap max-lg:gap-4">
+    <>
       {layout === "l-r" || screenWidth < 768 ? (
-        <>
+        <div className="flex max-lg:flex-wrap max-lg:gap-4">
           <img className="max-lg:w-full lg:w-1/2" src={src} alt={heading} />
           <div className="max-lg:w-full lg:w-1/2 px-8 flex flex-col justify-center text-center">
             <h2 className="text-3xl font-bold font-bebas-neue">{heading}</h2>
@@ -27,9 +27,9 @@ const WhyUs = ({ src, heading, description, highlights, btnTxt, layout }) => {
             </div>
             <button className="font-mozilla-headline">{btnTxt}</button>
           </div>
-        </>
+        </div>
       ) : (
-        <>
+        <div className="flex max-lg:flex-wrap max-lg:gap-4">
           <div className="max-lg:w-full lg:w-1/2 px-8 flex flex-col justify-center text-center">
             <h2 className="text-3xl font-bold font-bebas-neue">{heading}</h2>
             <div className="my-6 text-gray-900">
@@ -38,9 +38,9 @@ const WhyUs = ({ src, heading, description, highlights, btnTxt, layout }) => {
             <button className="font-mozilla-headline">{btnTxt}</button>
           </div>
           <img className="max-lg:w-full lg:w-1/2" src={src} alt={heading} />
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
