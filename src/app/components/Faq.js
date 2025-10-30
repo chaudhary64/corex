1311f5f442ad2a5dab83bf349087407b0be25406
chaudhary64 <chiragchoudhary64@gmail.com>
2025-10-30 19:12:03 +0530

    feat: add GSAP split text animations for responsive FAQ component

--- src/app/components/Faq.js
+++ src/app/components/Faq.js
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React, { useRef, useState } from "react";
+import { SplitText } from "gsap/SplitText";
+import React, { use, useRef, useState } from "react";
 import { FiPlus } from "react-icons/fi";
 
 const Faq = ({ id, question, answer }) => {
@@ -8,8 +9,10 @@ const Faq = ({ id, question, answer }) => {
 
   const [isToggle, setIsToggle] = useState(false);
 
+  const containerRef = useRef(null);
   const toggleBtnRef = useRef(null);
   const paraRef = useRef(null);
+  const borderRef = useRef(null);
 
   const toggle = () => setIsToggle((prev) => !prev);
 
@@ -35,24 +38,93 @@ const Faq = ({ id, question, answer }) => {
     }
   }, [isToggle]);
 
+  useGSAP(() => {
+    let mm = gsap.matchMedia();
+
+    mm.add("(width < 64rem)", () => {
+      const split = new SplitText(containerRef.current, {
+        type: "words,chars",
+        mask: "chars",
+      });
+
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top 95%",
+          end: "bottom 60%",
+        },
+      });
+
+      tl.from(split.chars, {
+        yPercent: 100,
+        stagger: 0.02,
+        ease: "power2.out",
+      }).from(
+        borderRef.current,
+        {
+          scaleX: 0,
+          transformOrigin: "left center",
+          duration: 1.75,
+          ease: "power2.out",
+        },
+        `<=${id * 0.1}`
+      );
+    });
+    mm.add("(width >= 64rem)", () => {
+      const split = new SplitText(containerRef.current, {
+        type: "words,chars",
+        mask: "chars",
+      });
+
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top 95%",
+          end: "bottom 60%",
+        },
+      });
+
+      tl.from(split.chars, {
+        yPercent: 100,
+        stagger: 0.02,
+        ease: "power2.out",
+      }).from(
+        borderRef.current,
+        {
+          scaleX: 0,
+          transformOrigin: "left center",
+          duration: 1.75,
+          ease: "power2.out",
+        },
+        `<=${id * 0.1}`
+      );
+    });
+  }, []);
+
   return (
-    <div className="border-b border-gray-500 py-4">
-      <div className="flex justify-between items-center">
-        <div>
-          <span>{number + ")"}</span>
-          &ensp;
-          <span>{question}</span>
+    <>
+      <div className="py-4">
+        <div ref={containerRef} className="flex justify-between items-center">
+          <div>
+            <span>{number + ")"}</span>
+            &ensp;
+            <span>{question}</span>
+          </div>
+          <FiPlus
+            ref={toggleBtnRef}
+            onClick={toggle}
+            className="text-lg cursor-pointer"
+          />
         </div>
-        <FiPlus
-          ref={toggleBtnRef}
-          onClick={toggle}
-          className="text-lg cursor-pointer"
-        />
+        <p ref={paraRef} className="mt-2 h-0 overflow-hidden hidden opacity-0">
+          {answer}
+        </p>
       </div>
-      <p ref={paraRef} className="mt-2 h-0 overflow-hidden hidden opacity-0">
-        {answer}
-      </p>
-    </div>
+      <aside
+        ref={borderRef}
+        className="border-b border-gray-500 w-full"
+      ></aside>
+    </>
   );
 };
 
