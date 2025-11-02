df8a4bf9bb280028d827c7cddca88dc19b87767b
chaudhary64 <chiragchoudhary64@gmail.com>
2025-11-02 23:01:42 +0530

    fix: ensure full width for Highlight component container

--- src/app/components/Highlight.js
+++ src/app/components/Highlight.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Highlight = ({ description, link }) => {
   return (
-    <div className="flex flex-col justify-between bg-white/75 shadow-lg rounded-xl p-6">
+    <div className="w-full flex flex-col justify-between bg-white/75 shadow-lg rounded-xl p-6">
       <p>{description}</p>
       <span className="mt-2 font-semibold underline underline-offset-1 cursor-pointer">
         {link}
