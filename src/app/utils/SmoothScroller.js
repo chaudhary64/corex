58b60e1fb4f2f672295b0fad117caf36fc0a4533
chirag chaudhary <chiragchoudhary64@gmail.com>
2026-06-06 14:08:50 +0530

    refactor(SmoothScroller): improve code readability and structure

--- src/app/utils/SmoothScroller.js
+++ src/app/utils/SmoothScroller.js
@@ -1,29 +1,49 @@
 "use client";
+
 import React, { useEffect, useRef } from "react";
 import { ReactLenis } from "lenis/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const SmoothScroller = ({ children }) => {
-  const lenisRef = useRef();
+gsap.registerPlugin(ScrollTrigger);
+
+export default function SmoothScroller({ children }) {
+  const lenisRef = useRef(null);
 
   useEffect(() => {
-    function update(time) {
+    const update = (time) => {
       lenisRef.current?.lenis?.raf(time * 1000);
-    }
+    };
 
     gsap.ticker.add(update);
+    gsap.ticker.lagSmoothing(0);
 
-    return () => gsap.ticker.remove(update);
+    const lenis = lenisRef.current?.lenis;
+
+    if (lenis) {
+      lenis.on("scroll", ScrollTrigger.update);
+    }
+
+    return () => {
+      gsap.ticker.remove(update);
+
+      if (lenis) {
+        lenis.off("scroll", ScrollTrigger.update);
+      }
+    };
   }, []);
+
   return (
     <ReactLenis
       root
-      options={{ autoRaf: false, wheelMultiplier: 0.5, touchMultiplier: 0.5 }}
       ref={lenisRef}
+      options={{
+        autoRaf: false,
+        wheelMultiplier: 0.5,
+        touchMultiplier: 0.5,
+      }}
     >
       {children}
     </ReactLenis>
   );
-};
-
-export default SmoothScroller;
+}
